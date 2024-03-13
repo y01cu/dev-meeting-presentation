9d71e5a0b5e28fab484d9a4fe6fd3af3f286c9cf
y01cu <74989036+y01cu@users.noreply.github.com>
2024-03-13 07:12:13 +0300

    Updated slide navigation system
    The system now has a decent simple animation with it that works seamlessly

--- Assets/Scripts/SlideManager.cs
+++ Assets/Scripts/SlideManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class SlideManager : MonoBehaviour
@@ -7,23 +8,52 @@ public class SlideManager : MonoBehaviour
     public GameObject[] slides;
     public int currentSlide = 0;
 
+    private bool isTransitioning = false;
+
     public void NextSlide()
     {
-        if (currentSlide < slides.Length - 1)
+        if (currentSlide < slides.Length - 1 && !isTransitioning)
         {
-            slides[currentSlide].SetActive(false);
-            currentSlide++;
-            slides[currentSlide].SetActive(true);
+            SetAsFull();
+            slides[currentSlide + 1].SetActive(true);
+            slides[currentSlide].transform.DOLocalMoveY(1200, 1, false).onComplete += () =>
+            {
+                slides[currentSlide].SetActive(false);
+                currentSlide++;
+                slides[currentSlide].SetActive(true);
+                SetAsEmpty();
+            };
         }
+    }
+
+    private void SetAsFull()
+    {
+        isTransitioning = true;
+    }
+
+    private void SetAsEmpty()
+    {
+        isTransitioning = false;
+    }
+
+    private IEnumerator AnimateSlideWithDirection(bool isForward)
+    {
+        yield return new WaitForSeconds(.5f);
+        NextSlide();
     }
 
     public void PreviousSlide()
     {
-        if (currentSlide > 0)
+        if (currentSlide > 0 && !isTransitioning)
         {
-            slides[currentSlide].SetActive(false);
-            currentSlide--;
-            slides[currentSlide].SetActive(true);
+            SetAsFull();
+            slides[currentSlide - 1].SetActive(true);
+            slides[currentSlide - 1].transform.DOLocalMoveY(0, 1, false).onComplete += () =>
+            {
+                slides[currentSlide].SetActive(false);
+                currentSlide--;
+                SetAsEmpty();
+            };
         }
     }
 }
